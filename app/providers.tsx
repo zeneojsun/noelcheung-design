344e2fdd201221e0de4465f3cb5c3e3cdbe05838
Noel Cheung <humanforhuman@Noels-MacBook-Air.local>
2026-05-10 22:50:14 +0900

    feat: set dark as default theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem={false}>
       {children}
     </ThemeProvider>
   );
